64e1cd128669628b3175b41dcfa9eef5f88ef5e9
Giang Minh <dominhgiang12@gmail.com>
2024-12-15 22:54:34 +0700

    config base axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import "./index.css";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -11,11 +11,16 @@ import Layout from './layout'
 import BookPage from './pages/client/book'
 import LoginPage from 'pages/client/auth/login'
 import RegisterPage from './pages/client/auth/register'
+import HomePage from './pages/client/home'
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
       {
         path: "/book",
         element: <BookPage />,
